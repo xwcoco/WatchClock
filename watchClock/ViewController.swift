aab056ca9d5f580ffe11af6f460fc018fbb4998b
徐卫 <xuwei@dfsoft.com.cn>
2018-11-08 10:10:22 +0800

    Initial Commit

---
+++ watchClock/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  watchClock
+//
+//  Created by 徐卫 on 2018/11/8.
+//  Copyright © 2018 徐卫. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
